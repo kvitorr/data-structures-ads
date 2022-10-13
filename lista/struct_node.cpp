50e07199739fe734aadefa512d6d9146077c49e6
kvitorr <kvitorrsantos@gmail.com>
2022-10-13 08:32:29 -0300

    Value and Reference Type

---
+++ lista/struct_node.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct Node2 {
+    int id;
+    Node2(){
+        id = 0;
+    }
+
+    void setNode2(int i){
+        id = i;
+    }
+
+    int getNode2(){
+        return id;
+    }
+};
+
+
+int main(){
+
+    struct Node2 y1;
+    struct Node2 y2;
+
+    y1.setNode2(3);
+    y2 = y1;
+    y2.setNode2(4);
+
+    cout << "(VALUE)y1.getNode2(): " << y1.getNode2() << endl;
+    cout << "(VALUE)y2.getNode2(): " << y2.getNode2() << endl;
+    cout << "-----------------------" <<endl;
+
+
+    return 0;
+}
